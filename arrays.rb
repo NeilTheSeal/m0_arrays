c74c64de17288778c249337224dafb0fb9497329
Neil Hendren <neil.hendren@gmail.com>
2023-07-10 21:19:57 -0600

    Add an array for each data type in Ruby

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+strings_array = ["zero", "one", "two", "three", "four"];
+integers_array = [0, 1, 2, 3, 4];
+floats_array = [0.0, 1.0, 2.0, 3.0, 4.0];
+bools_array = [true, false, false, false, true];
